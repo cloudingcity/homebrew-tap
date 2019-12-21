841fae35065c950cdbd7510e0e9b4477209b1ddc
goreleaserbot <goreleaser@carlosbecker.com>
2019-12-21 23:25:21 +0800

    Brew formula update for golab version v0.4.0

--- golab.rb
+++ golab.rb
@@ -2,20 +2,18 @@
 class Golab < Formula
   desc "A command-line tool for gitlab"
   homepage "https://github.com/cloudingcity/golab"
-  version "0.3.0"
+  version "0.4.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/cloudingcity/golab/releases/download/v0.3.0/golab_0.3.0_Darwin_x86_64.tar.gz"
-    sha256 "3df00e9ab016e25fc7ec4914236891df3980204ed321cafbf765aecd6c483f04"
+    url "https://github.com/cloudingcity/golab/releases/download/v0.4.0/golab_0.4.0_Darwin_x86_64.tar.gz"
+    sha256 "372a3e3fe63ae0b946b4ad2137cf72ab4e178b5a63475d55d3edb2ac6190daea"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/cloudingcity/golab/releases/download/v0.3.0/golab_0.3.0_Linux_x86_64.tar.gz"
-      sha256 "c6ce305d47a13759767b5da30e73932fbca695a2d1cda859b2eddeb300864cce"
+      url "https://github.com/cloudingcity/golab/releases/download/v0.4.0/golab_0.4.0_Linux_x86_64.tar.gz"
+      sha256 "15c89b6d95a9121bb59e188a33dd641f6066cf5bbed8434129c7341eb1641c99"
     end
   end
-  
-  depends_on "git"
 
   def install
     bin.install "golab"
